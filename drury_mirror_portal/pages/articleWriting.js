0f5a36c2cb09fd6cd385e6b8e1280bd32f34a5f0
Paul Pollard <ppollard@drury.edu>
2023-11-08 09:42:01 -0600

    Added Modal

--- drury_mirror_portal/pages/articleWriting.js
+++ drury_mirror_portal/pages/articleWriting.js
@@ -6,7 +6,10 @@ import styles from "../styles/article.module.css";
 import uploadStyles from "../styles/uploadImage.module.css";
 
 import { useRouter } from "next/router";
-import { Button, Box, Stack, Grid, Typography, Checkbox, Alert, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
+import { 
+		Button, Box, Stack, Grid, Typography, Checkbox, Alert, 
+		Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, 
+		Modal} from "@mui/material";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import CheckIcon from '@mui/icons-material/Check';
@@ -84,6 +87,7 @@ export default function articleWriting() {
 	const [noSelectedImgError, setNoSelectedImgError] = useState(false);
 	const [invalidFileTypeError, setInvalidFileTypeError] = useState(false);
 	const [saveWithoutImagePopup, setSaveWithoutImagePopup] = useState(false);
+	const [open, setOpen] = useState(false);
 
 	// Used to set the text on the submit button
 	const [buttonText, setButtonText] = useState("Save as Draft");
@@ -281,6 +285,14 @@ export default function articleWriting() {
 		setSaveWithoutImagePopup(false);
 	}
 
+	function openPreview() {
+		setOpen(true)
+	}
+
+	function closePreview() {
+		setOpen(false)
+	}
+
 	if (status === "authenticated") {
 		return (
 			<Box
@@ -445,6 +457,44 @@ export default function articleWriting() {
 							{buttonText}
 						</Button>
 
+						<Button
+							sx={{
+								m: 2,
+							}}
+							color="error"
+							variant="contained"
+							onClick={openPreview}
+						>
+							Preview Article
+						</Button>
+
+						<Modal
+						open={open}
+						onClose={closePreview}
+						aria-labelledby="modal-modal-title"
+						aria-describedby="modal-modal-description"
+						>
+						<Box 
+							sx={{
+								position: 'absolute',
+								top: '50%',
+								left: '50%',
+								transform: 'translate(-50%, -50%)',
+								width: 400,
+								bgcolor: 'background.paper',
+								border: '2px solid #000',
+								boxShadow: 24,
+								p: 4,
+							  }}>
+							<Typography id="modal-modal-title" variant="h6" component="h2">
+							Text in a modal
+							</Typography>
+							<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+							Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+							</Typography>
+						</Box>
+						</Modal>
+						
 						{/* {saveWithoutImagePopup ? 
 							<Dialog
 								open={saveWithoutImagePopup}
