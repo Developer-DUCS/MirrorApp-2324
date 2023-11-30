7ebe3b9e4a76ab037f1fad9145d245c61acdd8a4
charlieroder <charlieroder99@gmail.com>
2023-11-29 21:49:11 -0600

    new category table, added update/insert to save

--- drury_mirror_portal/pages/api/saveArticle.js
+++ drury_mirror_portal/pages/api/saveArticle.js
@@ -12,6 +12,7 @@ export default async (req, res) => {
 		let email = body.email;
 		let thumbnailImage = body.imageData;
 		let imageType = body.imageType;
+		let categories = [0, 1, 0, 0, 0, 0, 0];
 
 		if (check) {
 			check = "1";
@@ -24,41 +25,67 @@ export default async (req, res) => {
 		if (body.aid) {
 			let isDraft = check;
 			let aid = parseInt(body.aid);
-			saveQuery =
-				"UPDATE articles SET headline = ?, body = ?, isDraft = ?, imageType = ?, thumbnailImage = ? WHERE aid = ?";
 
-			const result = await executeQuery({
-				query: saveQuery,
+			// updating the article
+			let saveQueryArticle = `UPDATE articles SET headline = ?, body = ?, isDraft = ?, imageType = ?, thumbnailImage = ? WHERE aid = ?; `;
+
+			// updating the categories associated with the article
+			let saveQueryCategory = `UPDATE categories SET front_page = ?, sports = ?, lifestyle = ?, campus_news = ?, news = ?, weekend = ?, editorial = ? WHERE categories.aid = ?;`
+			
+			const articleResult = await executeQuery({
+				query: saveQueryArticle,
 				values: [headline, articleString, isDraft, imageType, thumbnailImage, aid],
 			});
-			console.log("🚀 ~ file: saveArticle.js:52 ~ result:", result);
-			if (result.error) {
+
+			const categoryResult = await executeQuery({
+				query: saveQueryCategory,
+				values: [categories[0], categories[1], categories[2], categories[3], categories[4], categories[5], categories[6], aid],
+			});
+
+			console.log("🚀 ~ file: saveArticle.js:52 ~ result:");
+			console.log("article query info: ", articleResult.info);
+			console.log("category query info: ", categoryResult.info);
+
+			if (articleResult.error || categoryResult.error) {
 				console.log("There was an error saving the article");
 				res.status(500).json({ error: "Unsuccessful Insertion" });
-			} else if (result.affectedRows == 1) {
+			} else if (articleResult.affectedRows == 1 && categoryResult.affectedRows == 1) {
 				console.log("Successfully saved the article");
 				res.status(201).json({ msg: "Successful Insertion" });
 			}
 		} else {
-			saveQuery =
-				"INSERT INTO articles(email,author, headline, body, isDraft, imageType, thumbnailImage, createdDate) VALUES(?,?,?,?,?,?,?, NOW())";
+			// inserting the article information
+			let articleQuery =
+				`INSERT INTO articles(email, author, headline, body, isDraft, imageType, thumbnailImage, createdDate) 
+				VALUES(?,?,?,?,?,?,?, NOW());`;
 
-			const result = await executeQuery({
-				query: saveQuery,
-				values: [
-					email,
-					author,
-					headline,
-					articleString,
-					check,
-					imageType,
-					thumbnailImage,
-				],
+			// inserting the category information associated with the article
+			let categoryQuery = 
+				`INSERT INTO categories(front_page, sports, lifestyle, campus_news, news, weekend, editorial, aid) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`
+			
+			const articleResult = await executeQuery({
+				query: articleQuery,
+				values: [email, author, headline, articleString, check, imageType, thumbnailImage],
 			});
 
-			if (result.error) {
+			// get the aid from the inserted article
+			const new_article = await executeQuery({
+				query: "SELECT aid FROM articles ORDER BY aid DESC LIMIT 1",
+			});
+
+			const categoryResult = await executeQuery({
+				query: categoryQuery,
+				values: [categories[0], categories[1], categories[2], categories[3], categories[4], categories[5], categories[6], new_article[0].aid],
+			});
+
+			if (articleResult.error || categoryResult.error) {
 				res.status(500).json({ error: "Unsuccessful Insertion" });
-			} else if (result.affectedRows == 1) {
+			} else if (articleResult.affectedRows == 1 && categoryResult.affectedRows == 1) {
+
+				console.log("🚀 ~ file: saveArticle.js:52 ~ result:");
+				console.log("article inserted with id: ", articleResult.insertId);
+				console.log("category values inserted with id: ", categoryResult.insertId);
+
 				res.status(201).json({ msg: "Successful Insertion" });
 			}
 		}
