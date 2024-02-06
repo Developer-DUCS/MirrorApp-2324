2e92ca1e1e3b553a2c5fe84745e8f61f80ff3aff
Cole <ccoombs002@drury.edu>
2024-02-06 12:17:12 -0600

    test 2, next.config

--- drury_mirror_portal/next.config.js
+++ drury_mirror_portal/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
-	//basePath: "/mirror",
+	basePath: "/mirror",
 	assetPrefix: "https://mcs.drury.edu/mirror/",
 	images: {
 		loader: "akamai",
