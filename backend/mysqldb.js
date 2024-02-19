28c314bf83829a09a5e5ad3fbecbb9d2a97839ba
= <=>
2024-02-19 09:37:44 -0600

    Removed older users and added du_mirror_dev for development

--- backend/mysqldb.js
+++ backend/mysqldb.js
@@ -19,63 +19,23 @@ const config = require("./mysqlconfig.json"); // Configuration file for the mysq
 // });
 
 // Change the myUser variable to your username
-const myUser = "andrew";
+const myUser = "du_mirror_dev";
 let db = "";
-if (myUser == "sam") {
-	db = mysql.createConnection({
-		host: config.sam_local_db[0].host,
-		user: config.sam_local_db[0].user,
-		password: config.sam_local_db[0].password,
-		database: config.sam_local_db[0].database,
-		port: config.sam_local_db[0].port,
-	});
-} else if (myUser == "daniel") {
-	db = mysql.createConnection({
-		host: config.daniel_local_db[0].host,
-		user: config.daniel_local_db[0].user,
-		password: config.daniel_local_db[0].password,
-		database: config.daniel_local_db[0].database,
-		port: config.daniel_local_db[0].port,
-	});
-} else if (myUser == "thomasN") {
-	db = mysql.createConnection({
-		host: config.thomasN_local_db[0].host,
-		user: config.thomasN_local_db[0].user,
-		password: config.thomasN_local_db[0].password,
-		database: config.thomasN_local_db[0].database,
-		port: config.thomasN_local_db[0].port,
-	});
-} else if (myUser == "thomasNWIN") {
-	db = mysql.createConnection({
-		host: config.thomasN_local_db_WIN[0].host,
-		user: config.thomasN_local_db_WIN[0].user,
-		password: config.thomasN_local_db_WIN[0].password,
-		database: config.thomasN_local_db_WIN[0].database,
-		port: config.thomasN_local_db_WIN[0].port,
-	});
-} else if (myUser == "haley") {
-	db = mysql.createConnection({
-		host: config.haley_local_db[0].host,
-		user: config.haley_local_db[0].user,
-		password: config.haley_local_db[0].password,
-		database: config.haley_local_db[0].database,
-		port: config.haley_local_db[0].port,
-	});
-} else if (myUser == "thomasO") {
-	db = mysql.createConnection({
-		host: config.thomasO_local_db[0].host,
-		user: config.thomasO_local_db[0].user,
-		password: config.thomasO_local_db[0].password,
-		database: config.thomasO_local_db[0].database,
-		port: config.thomasO_local_db[0].port,
-	});
-} else if (myUser == "du_mirror_user") {
+if (myUser == "du_mirror_user") {
 	db = mysql.createConnection({
 		host: config.du_mirror_user[0].host,
 		user: config.du_mirror_user[0].user,
 		password: config.du_mirror_user[0].password,
 		database: config.du_mirror_user[0].database,
 		port: config.du_mirror_user[0].port,
+	});
+} else if (myUser == "du_mirror_dev") {
+	db = mysql.createConnection({
+		host: config.du_mirror_dev[0].host,
+		user: config.du_mirror_dev[0].user,
+		password: config.du_mirror_dev[0].password,
+		database: config.du_mirror_dev[0].database,
+		port: config.du_mirror_dev[0].port,
 	});
 } else if (myUser == "charlie") {
 	db = mysql.createConnection({
